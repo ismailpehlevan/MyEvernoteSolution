3886023c0b3f48efe0558fff2e257b9935f80c7b
ISMAIL PEHLEVAN <ismailpehlevan@icloud.com>
2019-04-01 10:50:27 +0300

    First commit

---
+++ MyEvernote.Entities/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.Entities
+{
+    class Note : MyEntityBase
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public bool IsDraft { get; set; }
+        public int LikeCount { get; set; }
+
+    }
+}
